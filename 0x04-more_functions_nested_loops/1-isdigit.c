34c74f65340d0811e0ecea09d8f11ae1f8d142a3
123johnniee <githuajohn45@gmail.com>
2023-03-23 05:21:57 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,11 @@
  * @x: The number to be checked
  * Return: 1 for a character that will be a digit or 0 for any else
  */
-int _isdigit(int c)
+int _isdigit(int x)
 {
-        if (x >= 48 && x <= 57)
-        {
-        return (1);
-        }
-        return (0);
+	if (x >= 46 && x <= 57)
+	{
+	return (1);
+	}
+	return (0);
 }
