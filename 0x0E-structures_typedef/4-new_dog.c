b21b48beb50b5d2038ed2da6e9ccf6908053a177
123johnniee <githuajohn45@gmail.com>
2023-04-17 05:07:04 -0700

    Task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,3 +16,14 @@ i++;
 }
 return (i);
 }
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed by the dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
