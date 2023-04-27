c617c09a0117eaf10fad1f57432dd520fddf1c28
123johnniee <githuajohn45@gmail.com>
2023-04-27 05:38:32 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * @h: pointer to the list_t list
  * Return: number of elements
  */
-list_t *add_node(list_t **head, const char *str)
+size_t list_len(const list_t *h)
 {
 size_t n = 0;
 while (h)
