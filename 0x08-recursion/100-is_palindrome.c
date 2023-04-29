bcbea3b3cd5cd221c9aacd90b207bd123148b141
123johnniee <githuajohn45@gmail.com>
2023-04-29 03:00:26 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,7 +4,7 @@
  * @s: string
  * Return: the length of a string.
  */
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
 {
 if (*s == '\0')
 return (0);
@@ -20,10 +20,21 @@ else
  */
 int comparator(char *s, int n1, int n2)
 {
-if (8(s + n1) == *(s + n2))
+if (*(s + n1) == *(s + n2))
 {
 if (n1 == n2 || n1 == n2 + 1)
 return (0 + comparator(s, n1 + 1, n2 - n2 - 1));
 }
 return (0);
+}
+/**
+ * is_palindrome - detects if a string is a palindrome
+ * @s: string.
+ * Return: 1 if is a palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+if (*s == '\0')
+return (1);
+return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
