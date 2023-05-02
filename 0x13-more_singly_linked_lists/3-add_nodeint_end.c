8c6d7bdbe33e7ba83608f526875fc30ab7e1842a
123johnniee <githuajohn45@gmail.com>
2023-05-02 01:11:17 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * add_nodeint_end - Adds a new node at the 
+ * add_nodeint_end - Adds a new node at the
  * end of a listint_t list.
- * @head: A pointer to the address of the 
+ * @head: A pointer to the address of the
  * head of the listint_t list.
  * Return: If the function fails - NULL.
  * Otherwise - the address of the new elements.
