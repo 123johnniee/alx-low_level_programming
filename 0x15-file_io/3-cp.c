61f62270372f8f41801f967e390c999e8375b84f
123johnniee <githuajohn45@gmail.com>
2023-05-08 07:50:57 -0700

    TASK 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,11 +12,11 @@ void close_file(int fd);
 char *create_buffer(char *file)
 {
 char *buffer;
-buffer = malloc(sizeof(char) *1024);
+buffer = malloc(sizeof(char) * 1024);
 if (buffer == NULL)
 {
 dprintf(STDERR_FILENO,
-	"Error: Can't write to %s\n",file);
+	"Error: Can't write to %s\n", file);
 exit(99);
 }
 return (buffer);
@@ -27,12 +27,12 @@ return (buffer);
  */
 void close_file(int fd)
 {
-int c;	
+int c;
 c = close(fd);
 
 if (c == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n" , fd);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
 }
@@ -52,7 +52,7 @@ char *buffer;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit (97);
+exit(97);
 }
 buffer = create_buffer(argv[2]);
 from = open(argv[1], O_RDONLY);
@@ -62,7 +62,7 @@ do {
 	if (from == -1 || r == -1)
 	{
 	dprintf(STDERR_FILENO,
-		"Error: Can't read from file %s\n" , argv[1]);
+		"Error: Can't read from file %s\n", argv[1]);
 free(buffer);
 exit(98);
 }
@@ -70,7 +70,7 @@ w = write(to, buffer, r);
 if (to == -1 || w == -1)
 {
 dprintf(STDERR_FILENO,
-	"Error: Can't write to %s\n" , argv[2]);
+	"Error: Can't write to %s\n", argv[2]);
 free(buffer);
 exit(99);
 }
